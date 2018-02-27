a6ad40ceba7e85da17613179b7e827efe4f5ee28
Lyra Benson <lyra1255@gmail.com>
2018-02-27 11:35:38 -0500

    first prototype

---
+++ Assets/Scripts/Database/loginDB_script.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class loginDB_script : MonoBehaviour
+{
+    public string newUserURL = "http://rongkaiguo.com/ccsv/register_user.php";
+
+    public Button submitButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Start up");
+
+        Button b = GameObject.Find("Submit").GetComponent<Button>();
+        b.onClick.AddListener(submit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    private void submit()
+    {
+        Debug.Log("Submitted");
+        StartCoroutine(RunRegistration());
+
+        SceneManager.LoadScene("gameStart", LoadSceneMode.Single);
+    }
+
+
+    public IEnumerator RunRegistration()
+    {
+        Debug.Log("Registering User");
+        var form = new WWWForm();
+
+        var uname = GameObject.Find("Username").GetComponent<InputField>().text;
+        var pword = GameObject.Find("Password").GetComponent<InputField>().text;
+
+        Debug.Log(uname);
+        Debug.Log(pword);
+
+        form.AddField("username", uname);
+        form.AddField("password", pword);
+
+        var www = new WWW(newUserURL, form);
+        yield return www;
+
+        Debug.Log(www.text);
+
+
+    }
+}
---
+++ Assets/Scripts/Player/playerControllerScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerControllerScript : MonoBehaviour {
+
+    public float speed;
+
+    private Vector2 target;
+
+    private Rigidbody2D rb2d;
+    private Camera Camera;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        target = transform.position;
+    }
+
+    void PlayerPosition()   // Set Player Position.X to always attempt to mirror MousePos.X
+    {
+        if (true)
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //target.y = transform.position.y;
+            //target.x = transform.position.x;
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        PlayerPosition();	
+	}
+}
