57bb3599259d1b1090ce908af8af7da458bc5e75
Lyra Benson <lyra1255@gmail.com>
2018-03-05 14:41:24 -0500

    json test

---
+++ Assets/Scripts/Database/jsonTestScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[Serializable]
+public class jsonTestScript : MonoBehaviour {
+
+    public int level;
+    public float timeElapsed;
+    public string playerName;
+
+    jsonTestScript myObject = new jsonTestScript();
+    myObject.level = 1;
+    myObject.timeElapsed = 47.5f;
+    myObject.playerName = "Dr Charles Francis";
+
+    // Use this for initialization
+    void Start() {
+        string json = JsonUtility.ToJson(myObject);
+        myObject = JsonUtility.FromJson<jsonTestScript>(json);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
